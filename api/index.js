3668f19c88234b448e96428653af437d502f74ee
mluciasolis <magda_lucia97@hotmail.com>
2019-08-24 23:19:18 -0500

    Top de clientes

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const { Router } = require('express')
 const router = Router()
 const loginApi = require('./auth/login.js')
 const { clienteApi, mejorCliente, gastoGrande, proovedorGrande, ivaCobrado, ivaPagado } = require('./aeq/perfil.js')
+const{topClientes}=require('./lucia/cliente.js')
     /*const refresher = require( './auth/refresher.js')
     const { register, verifyUnique } = require('./auth/register.js')*/
 
@@ -24,5 +25,7 @@ router.get('/proovedorGrande', proovedorGrande)
 router.get('/ivaCobrado', ivaCobrado)
 
 router.get('/ivaPagado', ivaPagado)
+
+router.get('/topClientes', topClientes)
 
 module.exports = router;
